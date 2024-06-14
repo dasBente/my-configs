88cb4f23fce0c50c02aa8bab5594bee7195b8117
dasBente <dasbente@gmail.com>
2024-06-14 16:29:27 +0200

    feat: set keybinds for lsp-zero

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -25,17 +25,45 @@ cmp.setup({
 lsp.on_attach(function(_, bufnr)
 	lsp.default_keymaps({ buffer = bufnr })
 
-	--local opts = {buffer, bufnr, remap = false}
-	--vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	--vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	--vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	--vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-	--vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	--vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-	--vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	--vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-	--vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	--vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("n", "gd",
+		function() vim.lsp.buf.definition() end,
+		{ buffer = bufnr, desc = "Definition" })
+
+	vim.keymap.set("n", "K",
+		function() vim.lsp.buf.hover() end,
+		{ buffer = bufnr })
+
+	vim.keymap.set("n", "<leader>vw",
+		function() vim.lsp.buf.workspace_symbol() end,
+		{ buffer = bufnr, desc = "Workspace Symbols" })
+
+	vim.keymap.set("n", "<leader>vd",
+		function() vim.diagnostic.open_float() end,
+		{ buffer = bufnr, desc = "Diagnostic" })
+
+	vim.keymap.set("n", "[d",
+		function() vim.diagnostic.goto_next() end,
+		{ buffer = bufnr, desc = "Next Diagnostic" })
+
+	vim.keymap.set("n", "]d",
+		function() vim.diagnostic.goto_prev() end,
+		{ buffer = bufnr, desc = "Prev Diagnostic" })
+
+	vim.keymap.set("n", "<leader>vc",
+		function() vim.lsp.buf.code_action() end,
+		{ buffer = bufnr, desc = "Code Action" })
+
+	vim.keymap.set("n", "<leader>vv",
+		function() vim.lsp.buf.references() end,
+		{ buffer = bufnr, desc = "References" })
+
+	vim.keymap.set("n", "<leader>vrn",
+		function() vim.lsp.buf.rename() end,
+		{ buffer = bufnr)
+
+	vim.keymap.set("i", "<C-h>",
+		function() vim.lsp.buf.signature_help() end,
+		{ buffer = bufnr })
 end)
 
 require("mason").setup({})
